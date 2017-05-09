cdadaa6750ea2a81046015ad3e32d8311519f078
Marko Ludlow and Nick Powell <nick@>
2017-05-09 11:29:35 -0700

    add working update and delete functionality to page apprb

--- app.rb
+++ app.rb
@@ -26,7 +26,25 @@ post("/add_survey") do
   erb(:surveys)
 end
 
+
+# path to individual survey
 get("/survey/:id") do
   @survey = Surveys.find(params['id'].to_i)
   erb(:survey)
+end
+
+
+# path to update survey name
+patch("/edit_survey/:id") do
+  new_title = params['edit-title']
+  survey = Surveys.find(params['id'].to_i)
+  survey.update(title: new_title)
+  redirect("/survey/#{survey.id}")
+end
+
+# path to delete a survey
+delete("/delete_survey/:id") do
+  survey = Surveys.find(params['id'].to_i)
+  survey.delete
+  redirect("/surveys")
 end
